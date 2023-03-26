e532c3418342e6a83e21bdadd156f64e32efd883
MeParas325 <adamchacha398@gmail.com>
2023-03-26 12:36:30 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+import NavBar from './components/NavBar';
+import News from './components/News';
+// import { useState } from 'react';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom"
+
+
+// const toggleMode = () =>{
+//   if(mode === "light"){
+//     setMode("dark")
+//   }
+//   else{
+//     setMode("light")
+//   }
+// }
+
+export default class App extends Component {
+  
+  // constructor(){
+  //   this.state = {
+
+  //   }
+  // }
+  
+  
+  
+  render() {
+    // const [mode, setMode] = useState("light")
+    return (
+      <>
+      <BrowserRouter>
+      <NavBar />
+      <Routes>
+       <Route key="home"  exact path='/' element={<News country="us" category="general" />}></Route>
+       <Route key="business" exact path='/business'  element={<News country="us" category="business" />}></Route>
+       <Route key="entertainment"  exact path='/entertainment' element={<News country="us" category="entertainment" />}></Route>
+       <Route key="health"  exact path='/health' element={<News country="us" category="health" />}></Route>
+       <Route key="science"  exact path='/science' element={<News country="us" category="science" />}></Route>
+       <Route key="sports"  exact path='/sports' element={<News country="us" category="sports" />}></Route>
+       <Route key="technology"  exact path='/technology' element={<News country="us" category="technology" />}></Route>
+      </Routes>
+      </BrowserRouter>
+      </>
+    )
+  }
 }
 
-export default App;
---
+++ src/components/News.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+import PropTypes from 'prop-types'
+
+export class News extends Component {
+  static defaultProps = {
+    country: "in",
+    category: "business"
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    category: PropTypes.string
+  }
+  i = 0
+  constructor(){
+    super()
+    this.state = {
+      articles: [],
+      page: 1,
+      totalResults: 0
+    }
+  }
+
+  async componentDidMount(){
+    let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=3671f58f31eb4c94acfb56fe503a617b&pageSize=20`);
+    // console.log(typeof data)
+    // console.log(data)
+    let parsedData = await data.json()
+    // console.log(typeof parsedData)
+    // console.log(parsedData)
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parseInt(parsedData.totalResults)
+      
+    })
+
+  }
+
+  handleNextClick = async () =>{
+      let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=3671f58f31eb4c94acfb56fe503a617b&pageSize=20&page=${this.state.page + 1}&pageSize=20`);
+      let parsedData = await data.json()
+    
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles
+    
+      })
+
+    
+
+
+
+  }
+
+  handlePreviousClick = async () =>{
+
+    let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=3671f58f31eb4c94acfb56fe503a617b&pageSize=20&page=${this.state.page - 1}&pageSize=20`);
+    let parsedData = await data.json()
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles
+
+    })
+
+
+  }
+  render() {
+    // console.log(this.state.articles)
+    return (
+        <>
+      <div className='container my-3'>
+        <h2 className='text-center'>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+          {this.state.articles.map((element)=>{
+            return <div className="col-md-4" key={this.i++}>
+            <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url}/>
+         </div>
+
+          })}
+
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page<=1} type='button' onClick={this.handlePreviousClick} className='btn btn-dark'>&larr; Previous</button>
+          <button disabled={this.state.page >= Math.ceil(this.state.totalResults/20)} type='button' onClick={this.handleNextClick} className='btn btn-dark'>Next &rarr;</button>
+        </div>
+      </div>
+      </>
+
+    )
+  }
+}
+
+export default News
